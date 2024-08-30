32d0018ed7b907996f463b42c14aa8da0742f446
Mel-Rocha <rochamel73@gmail.com>
2024-08-29 23:28:31 -0300

    categorias de alimentos

--- apps/taco/urls.py
+++ apps/taco/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CMVColtaco3ListView, CMVColtaco3DetailView
+from .views import CMVColtaco3ListView, CMVColtaco3DetailView, CMVColtaco3CategoryView
 
 urlpatterns = [
     path('taco/', CMVColtaco3ListView.as_view(), name='taco-list'),
     path('taco/<str:param>/', CMVColtaco3DetailView.as_view(), name='taco-detail'),
+    path('taco/category/<str:category>/', CMVColtaco3CategoryView.as_view(), name='taco-category'),
 ]
--- apps/taco/views.py
+++ apps/taco/views.py
@@ -61,3 +61,25 @@ class CMVColtaco3DetailView(APIView):
 
         except Exception as e:
             return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class CMVColtaco3CategoryView(APIView):
+    def get(self, request, category):
+        query = "SELECT * FROM CMVColtaco3 WHERE category = %s"
+        params = [category]
+
+        # Executando a query no banco de retenção
+        with get_retention_db_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, params)
+                rows = cursor.fetchall()
+
+                # Captura a descrição das colunas
+                columns = [col[0] for col in cursor.description]
+
+        # Convertendo os resultados para uma lista de dicionários
+        foods = [dict(zip(columns, row)) for row in rows]
+
+        # Serializando os dados
+        serializer = CMVColtaco3Serializer(foods, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
