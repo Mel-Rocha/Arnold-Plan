3a32b5c0753c7415ccc7477e0e01edbee9b82fa9
Mel-Rocha <rochamel73@gmail.com>
2024-09-01 14:31:50 -0300

    deixando mais clean

--- apps/macros_sheet/serializers.py
+++ apps/macros_sheet/serializers.py
@@ -2,23 +2,17 @@ from rest_framework import serializers
 from apps.macros_sheet.models import MealMacrosSheet, DietMacrosSheet
 
 
+MACROS_SHEETS_FIELDS = [
+    'id', 'cho', 'ptn', 'fat', 'kcal', 'cho_proportion', 'ptn_proportion', 'fat_proportion',
+    'cho_level', 'ptn_level', 'fat_level', 'kcal_level'
+]
+
 class MealMacrosSheetSerializer(serializers.ModelSerializer):
-    cho = serializers.ReadOnlyField()
-    ptn = serializers.ReadOnlyField()
-    fat = serializers.ReadOnlyField()
-    kcal = serializers.ReadOnlyField()
-    cho_proportion = serializers.ReadOnlyField()
-    ptn_proportion = serializers.ReadOnlyField()
-    fat_proportion = serializers.ReadOnlyField()
-    cho_level = serializers.ReadOnlyField()
-    ptn_level = serializers.ReadOnlyField()
-    fat_level = serializers.ReadOnlyField()
-    kcal_level = serializers.ReadOnlyField()
 
     class Meta:
         model = MealMacrosSheet
-        fields = ['id', 'meal', 'cho', 'ptn', 'fat', 'kcal', 'cho_proportion', 'ptn_proportion', 'fat_proportion',
-                  'cho_level', 'ptn_level', 'fat_level', 'kcal_level']
+        fields = MACROS_SHEETS_FIELDS
+        read_only_fields = MACROS_SHEETS_FIELDS
 
 
 class DietMacrosSheetSerializer(serializers.ModelSerializer):
