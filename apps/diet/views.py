c134ba689f0e1bbf11825581aa8926979490bdb1
Mel-Rocha <rochamel73@gmail.com>
2024-09-16 19:32:11 -0300

    rodou

--- apps/diet/views.py
+++ apps/diet/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from openpyxl.styles import Border, Side, Alignment, Font
+from openpyxl.styles import Border, Side, Alignment, Font, PatternFill
 
 from apps.diet.models import Diet
 from apps.user.models import Athlete
@@ -59,23 +59,35 @@ class DietViewSet(AthleteNutritionistPermissionMixin):
         # Remover as linhas de grade
         ws.sheet_view.showGridLines = False
 
+        # Definindo estilos de preenchimento de cores
+        orange_fill = PatternFill(start_color="FFA500", end_color="FFA500", fill_type="solid")
+        yellow_fill = PatternFill(start_color="FFFF99", end_color="FFFF99", fill_type="solid")
+        green_fill = PatternFill(start_color="CCFFCC", end_color="CCFFCC", fill_type="solid")
+        blue_fill = PatternFill(start_color="ADD8E6", end_color="ADD8E6", fill_type="solid")
+        pink_fill = PatternFill(start_color="FFB6C1", end_color="FFB6C1", fill_type="solid")
+
+        # Estilos de borda e alinhamento
+        thin_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'),
+                             bottom=Side(style='thin'))
+        center_alignment = Alignment(horizontal='center', vertical='center')
+
         # Adicionando meta, observações e datas
-        ws['A1'] = "Meta"
-        ws['A2'] = "Observações"
-        ws['A3'] = "Data Inicial"
-        ws['A4'] = "Data Final"
-
-        # Mesclando as células da coluna A e B
-        ws.merge_cells('A1:B1')
-        ws.merge_cells('A2:B2')
-        ws.merge_cells('A3:B3')
-        ws.merge_cells('A4:B4')
-
-        # Preenchendo as informações na célula mesclada (A) após a mesclagem
         ws['A1'] = f"Meta: {diet.goal}"
         ws['A2'] = f"Observações: {diet.observations}"
         ws['A3'] = f"Data Inicial: {diet.initial_date.strftime('%Y-%m-%d')}"
         ws['A4'] = f"Data Final: {diet.final_date.strftime('%Y-%m-%d')}"
+
+        # Mesclando as células da coluna A e B
+        ws.merge_cells('A1:I1')
+        ws.merge_cells('A2:I2')
+        ws.merge_cells('A3:I3')
+        ws.merge_cells('A4:I4')
+
+        # Estilos das células de Meta e Observações
+        for cell in ['A1', 'A2', 'A3', 'A4']:
+            ws[cell].alignment = center_alignment
+            ws[cell].border = thin_border
+            ws[cell].fill = orange_fill
 
         # Espaçamento para cabeçalho da tabela de alimentos
         ws.append([""])
@@ -87,21 +99,16 @@ class DietViewSet(AthleteNutritionistPermissionMixin):
         # Estilo dos cabeçalhos
         header_font = Font(bold=True)
         for col in range(1, len(headers) + 1):
-            ws.cell(row=6, column=col).font = header_font
+            header_cell = ws.cell(row=6, column=col)
+            header_cell.font = header_font
+            header_cell.alignment = center_alignment
+            header_cell.border = thin_border
+            header_cell.fill = yellow_fill
 
-        # Ajustando largura das colunas
+        # Definindo largura das colunas
         ws.column_dimensions['A'].width = 20
         ws.column_dimensions['B'].width = 15
         ws.column_dimensions['C'].width = 40
-
-        # Estilo das bordas
-        thin_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'),
-                             bottom=Side(style='thin'))
-
-        # Estilos das fontes
-        font_protein = Font(color="FF69B4")  # Rosa
-        font_carbs = Font(color="00FF00")  # Verde
-        font_fat = Font(color="FFFF00")  # Amarelo
 
         row_num = 7
         for meal in diet.meals.all():
@@ -143,7 +150,13 @@ class DietViewSet(AthleteNutritionistPermissionMixin):
                 for row in range(first_row, row_num):
                     cell = ws.cell(row=row, column=col)
                     cell.border = thin_border
-                    cell.alignment = Alignment(horizontal='center', vertical='center')
+                    cell.alignment = center_alignment
+
+                # Aplicando cor de fundo às células de dados
+                if meal.name.lower() == "café da manhã":
+                    ws.cell(row=row_num - 1, column=1).fill = pink_fill
+                else:
+                    ws.cell(row=row_num - 1, column=1).fill = blue_fill
 
             # Adicionar informações do MealMacrosSheet
             meal_macros = meal.meal_macros_sheet
@@ -166,11 +179,11 @@ class DietViewSet(AthleteNutritionistPermissionMixin):
         ws.append(["", "", "Proporções", f"CHO: {diet_macros.cho_proportion}%", f"PTN: {diet_macros.ptn_proportion}%",
                    f"FAT: {diet_macros.fat_proportion}%"])
 
-        # Adicionar uma linha em branco após os macros da dieta
-        ws.append([""])
-        row_num += 1
-
         # Aplicar cores às fontes dos valores de macro
+        font_protein = Font(color="FF69B4")  # Rosa
+        font_carbs = Font(color="00FF00")  # Verde
+        font_fat = Font(color="FFFF00")  # Amarelo
+
         for row in ws.iter_rows(min_row=7, min_col=4, max_col=9):
             for cell in row:
                 if 'CHO' in ws.cell(row=6, column=cell.column).value:
